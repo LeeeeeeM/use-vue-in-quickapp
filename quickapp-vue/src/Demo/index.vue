afff1b4d8c54c901bba124d3be785a86b918c87f
LeeeeeeM <sunliangmu@xiaomi.com>
2019-07-31 15:19:33 +0800

    add less bem

--- quickapp-vue/src/Demo/index.vue
+++ quickapp-vue/src/Demo/index.vue
@@ -2,6 +2,8 @@
   <!-- template里只能有一个根节点 -->
   <div class="demo-page">
     <text class="title">欢迎打开{{title}}</text>
+    <input class="btn" type="button" value="随机变化Item位置" @click="changePosition(list)" />
+    <text-shower v-for="(item) in list" :key="item.id" :config="item"></text-shower>
     <!-- 点击跳转详情页 -->
     <input class="btn" type="button" value="跳转到详情页" @click="routeDetail" />
   </div>
@@ -9,11 +11,42 @@
 
 <script>
   import router from '@system.router'
-
+  import textShower from './text-shower'
   export default {
+    components: {
+      textShower
+    },
     // 页面级组件的数据模型，影响传入数据的覆盖机制：private内定义的属性不允许被覆盖
     private: {
-      title: '示例页面'
+      title: '示例页面',
+      list: []
+    },
+    created() {
+      this.list = [{
+        id: 1,
+        name: 'vue',
+        use: 'framework'
+      }, {
+        id: 2,
+        name: 'vuex',
+        use: 'store'
+      }, {
+        id: 3,
+        name: 'immer',
+        use: 'immutable'
+      }, {
+        id: 4,
+        name: 'element',
+        use: 'ui'
+      }]
+    },
+    mounted() {
+      this.timer = setInterval(() => {
+        this.changePosition(this.list)
+      }, 1000)
+    },
+    beforeDestroy() {
+      clearInterval(this.timer)
     },
     methods: {
       async routeDetail () {
@@ -21,6 +54,9 @@
         router.push ({
           uri: '/DemoDetail'
         })
+      },
+      changePosition(list) {
+        list.sort(() => Math.random() > 0.5)
       }
     }
   }
---
+++ quickapp-vue/src/Demo/text-shower.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <text @click="clickItem" class="text-shower-title" :class="`text-shower-title__${clicked ? 'red' : 'green'}`">{{config.name}} ---- {{config.use}}</text>
+  </div>
+</template>
+<script>
+export default {
+  props: ['config'],
+  data() {
+    return {
+      clicked: false
+    }
+  },
+  methods: {
+    clickItem() {
+      this.clicked = !this.clicked
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  // 添加scope-name
+  @component-name: text-shower;
+  @text-color: blueviolet;
+  @bg-red: #ff0000;
+  @bg-green: #00ffff;
+
+  .@{component-name}-title {
+    color: @text-color;
+    margin: 30px 0 10px;
+    height: 50px;
+    &__red {
+      background-color: @bg-red;
+    }
+    &__green {
+      background-color: @bg-green;
+    }
+  }
+</style>
